1cb75ad472001133a0b395608e19e13d18687b66
Huy-Long Nguyen <huy.long.nguyen26@gmail.com>
2015-04-28 20:46:03 +0200

    added tests for opening comparison popup

---
+++ tests/functional/index-web-comparison.js
@@ -0,0 +1,75 @@
+define([
+    'intern!object',
+    'intern/chai!assert',
+    'require'
+], function (registerSuite, assert, require) {
+    registerSuite({
+        name: 'index',
+
+        // before the suite starts
+        setup: function () {
+            return this.remote
+                .get(require.toUrl('http://bnr.storage.googleapis.com/rechner-brutto-netto/index~web.html'))
+                .setFindTimeout(5000)
+                .findById('shared_widgets_NumberInput_0')
+                    .click()
+                    .type('2000')
+                    .pressKeys('\uE007')
+                    .end()
+                .findById('shared_widgets_NumberInput_1')
+                    .click()
+                    .type('1800')
+                    .pressKeys('\uE007')
+                    .end()
+                .findById('shared_widgets_Link_3')
+                .click();
+        },
+
+        'III-V': function () {
+            return this.remote
+                .findById('shared_widgets_NumberOutput_34')
+                .getVisibleText()
+                .then(function (text) {
+                    assert.strictEqual(text, '450,71',
+                        'III-V should be 450,71 for brutto 2000-1800');
+                });
+        },
+
+        'V-III': function () {
+            return this.remote
+                .findById('shared_widgets_NumberOutput_45')
+                .getVisibleText()
+                .then(function (text) {
+                    assert.strictEqual(text, '491,60',
+                        'V-III should be 491,60 for brutto 2000-1800');
+                });
+        },
+
+        'IV-IV': function () {
+            return this.remote
+                .findById('shared_widgets_NumberOutput_46')
+                .getVisibleText()
+                .then(function (text) {
+                    assert.strictEqual(text, '418,89',
+                        'IV-IV should be 418,89 for brutto 2000-1800');
+                });
+        },
+
+        'IV-IV-2': function () {
+            return this.remote
+                .findById('shared_widgets_NumberOutput_48')
+                .getVisibleText()
+                .then(function (text) {
+                    assert.strictEqual(text, '418,89',
+                        'IV-IV F=1000 should be 418,89 for brutto 2000-1800');
+                });
+        },
+
+        'terminate': function () {
+            return this.remote
+                .findByClassName('logo calc')
+                    .click;
+        }
+
+    });
+});
--- tests/intern.js
+++ tests/intern.js
@@ -63,7 +63,11 @@ define({
 	suites: [ /*'tests/unit/hello'*/ ],
 
 	// Functional test suite(s) to run in each browser once non-functional tests are completed
-	functionalSuites: [ 'tests/functional/index-web',  'tests/functional/index-web_rev'],
+	functionalSuites: [
+		//'tests/functional/index-web',
+		//'tests/functional/index-web_rev',
+		'tests/functional/index-web-comparison'
+	],
 
 	// A regular expression matching URLs to files that should not be included in code coverage analysis
 	excludeInstrumentation: /^(?:tests|node_modules)\//
