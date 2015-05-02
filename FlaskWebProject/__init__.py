ea6732a3dbf71bba2591f3f1a6c1caa9f6818651
Huy-Long Nguyen <huy.long.nguyen26@gmail.com>
2015-05-02 12:00:49 +0200

    added database models with User class

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -4,8 +4,10 @@ The flask application package.
 
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.bcrypt import Bcrypt
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 import FlaskWebProject.views
---
+++ FlaskWebProject/models.py
@@ -0,0 +1,31 @@
+from FlaskWebProject import db
+from FlaskWebProject import bcrypt
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password = db.Column(db.String)
+
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = bcrypt.generate_password_hash(password)
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        try:
+            return unicode(self.id)  # python 2
+        except NameError:
+            return str(self.id)  # python 3
+
+    def __repr__(self):
+        return '<User %r>' % (self.username)
