f86303290ecda6783853ce6a95f6a0e6aa04a239
Konstantin Keuchel <konstantin.keuchel@fau.de>
2015-05-08 13:03:15 +0200

    adapt config files for db in aws

--- config-aws-example.py
+++ config-aws-example.py
@@ -6,7 +6,7 @@ SECRET_KEY = 'db_secret'
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://<Username>:<Password>@amos-proj.ciuf2mb9oqh6.us-west-2.rds.amazonaws.com:3306/amosDB'
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
 sso_fb_consumer_key = "fb_id"
