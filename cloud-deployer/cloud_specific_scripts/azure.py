b7859320d1f320234ceeef5325ee3996acfbb4bf
Richard Baumann <thekilleddeath@endless-aerospace.de>
2015-05-16 21:52:51 +0200

    azure deployment working

--- cloud-deployer/cloud_specific_scripts/azure.py
+++ cloud-deployer/cloud_specific_scripts/azure.py
@@ -3,6 +3,8 @@ __author__ = 'ohaz'
 import os
 import util
 import shutil
+import subprocess
+import distutils
 
 OWN_FOLDER = os.path.join(util.OWN_PATH, 'cloud_specific_files', 'azure')
 
@@ -12,6 +14,13 @@ add_to_reqs = ['azure']
 
 from util import copy_repo_to_specific, add_to_requirements
 
+def run_subprocess(cmd):
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stdin=subprocess.PIPE, bufsize=1)
+    for line in iter(p.stdout.readline, b''):
+        print(line)
+    p.stdout.close()
+    p.wait()
+
 # Implement this function
 def deploy():
     try:
@@ -19,6 +28,15 @@ def deploy():
     except StandardError, e:
         print('Canceling Azure')
         return
+    print(' > > Copying downloaded files into repo')
+    distutils.dir_util.copy_tree(os.path.join(OWN_FOLDER, 'repo'), os.path.join(OWN_FOLDER, 'azure_repo'))
+    print(' > > Doing a git commit')
+    command = ['git', '--git-dir='+os.path.join(OWN_FOLDER,'azure_repo', '.git/'), '--work-tree='+os.path.join(OWN_FOLDER, 'azure_repo/'), 'commit', '-am', '"Autodeploy with deployer script"']
+    run_subprocess(command)
+    print(' > > Pushing')
+    command = ['git', '--git-dir='+os.path.join(OWN_FOLDER,'azure_repo', '.git/'), '--work-tree='+os.path.join(OWN_FOLDER, 'azure_repo/'), 'push']
+    run_subprocess(command)
+    print(' > > Done Pushing')
 
 def prepare_deployment():
     get_azure_git()
@@ -34,18 +52,13 @@ def get_azure_git():
     to = os.path.join(OWN_FOLDER, 'azure_repo')
     if os.path.exists(to):
         shutil.rmtree(to)
-    import subprocess
     command = ['git', 'clone', url, to]
     git_output = ''
     try:
         print('> > Cloning Azure repo. This might take some time...')
-        p = subprocess.Popen(command, stdout=subprocess.PIPE, bufsize=1)
-        for line in iter(p.stdout.readline, b''):
-            print (line)
-        p.stdout.close()
-        p.wait()
+        run_subprocess(command)
     except Exception, e:
-        print('Git checkout from azure failed. Stacktrace: ')
+        print('Git checkout from Azure failed. Stacktrace: ')
         print(e)
         raise StandardError()
     print(git_output)
