218b4170dbc2c11fd0268ab36070880cfb1e474a
Richard Baumann <ohaz@users.noreply.github.com>
2015-05-19 21:13:15 +0200

    Storageinterface now only imports needed cloud

--- FlaskWebProject/storageinterface.py
+++ FlaskWebProject/storageinterface.py
@@ -1,40 +1,51 @@
 from config import cloudplatform
-import azurestorage
-import googlestorage
-import awsstorage
+#import azurestorage
+#import googlestorage
+#import awsstorage
 
+storage_adapter = None
 
-def get_storage_adapter():
-    if cloudplatform == "google":
-        return googlestorage
-    elif cloudplatform == "aws":
-        return awsstorage
-    elif cloudplatform == "azure":
-        return azurestorage
+if cloudplatform == "google":
+    import googlestorage
+    storage_adapter = googlestorage
+elif cloudplatform == "aws":
+    import awsstorage
+    storage_adapter = awsstorage
+elif cloudplatform == "azure":
+    import azurestorage
+    storage_adapter = azurestorage
+
+#def get_storage_adapter():
+#    if cloudplatform == "google":
+#        return googlestorage
+#    elif cloudplatform == "aws":
+#        return awsstorage
+#    elif cloudplatform == "azure":
+#        return azurestorage
 
 #
 # Create a new container for files
 #
 def create_container(userID):
-    return get_storage_adapter().create_container(userID)
+    return storage_adapter.create_container(userID)
 
 def container_exists(container):
-    return get_storage_adapter().container_exists(container)
+    return storage_adapter.container_exists(container)
 
 def file_exists(container, filename):
-    return get_storage_adapter().file_exists(container, filename)
+    return storage_adapter.file_exists(container, filename)
 
 def list_files(container):
-    return get_storage_adapter().list_files(container)
+    return storage_adapter.list_files(container)
 
 def delete_file(container, filename):
-    get_storage_adapter().delete_file(container, filename)
+    storage_adapter.delete_file(container, filename)
 
 def delete_container(container):
-    get_storage_adapter().delete_container(container)
+    storage_adapter.delete_container(container)
 
 def upload_from_path(container, path):
-    get_storage_adapter().upload_from_path(container, path)
+    storage_adapter.upload_from_path(container, path)
 
 def upload_from_text(container, filename, text):
-    get_storage_adapter().upload_from_text(container, filename, text)
+    storage_adapter.upload_from_text(container, filename, text)
