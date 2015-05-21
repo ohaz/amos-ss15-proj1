149b2a1d83a5fb12f5c84d47e44056d7bc30f6df
Konstantin Keuchel <konstantin.keuchel@fau.de>
2015-05-21 19:14:05 +0200

    add comments to awsstorage.py

--- FlaskWebProject/awsstorage.py
+++ FlaskWebProject/awsstorage.py
@@ -6,11 +6,14 @@ import ntpath
 
 #
 # Connection to AWS S3
+# Create a connection to S3 with user credentials
+# Default Storage point is in the US. It`s possible to change it
 #
 s3_conn = S3Connection(AWS_S3_ACCESS_KEY, AWS_S3_ACCESS_SECRET)
 
 #
 # Create a new container for files
+# Overwrite existing bucket with the same name in own namespace
 #
 def create_container(bucket):
     ret_val = False
@@ -34,13 +37,13 @@ def container_exists(bucket):
     bucketname = bucketname.lower()
     return bucketname in [x.name for x in s3_conn.get_all_buckets()]
 
+# return a list with the names of all files in the bucket 
 def list_files(bucket):
     bucketname = AWS_S3_ACCESS_KEY + "_" + bucket
     bucketname = bucketname.lower()
     bucket_content = s3_conn.get_bucket(bucketname)
     bucket_files = [x.name for x in bucket_content]    
     return bucket_files
-
 
 def file_exists(bucket, filename):
     bucketname = AWS_S3_ACCESS_KEY + "_" + bucket
@@ -64,6 +67,7 @@ def file_change_permissions(bucket, filename, permission):
 # UPLOAD FILES
 #
 
+# create a file with the specific filename and upload the string into
 def upload_from_text(bucket, filename, text):
     bucketname = AWS_S3_ACCESS_KEY + "_" + bucket
     bucketname = bucketname.lower()
@@ -73,6 +77,9 @@ def upload_from_text(bucket, filename, text):
     k.set_contents_from_string(text)
     return True
 
+# upload a file from specific path into a bucket
+# before uploading the data, it will be checked if the file exists
+# if the file exists the upload will fail
 def upload_from_path(bucket, path):
     bucketname = AWS_S3_ACCESS_KEY + "_" + bucket
     bucketname = bucketname.lower()
@@ -90,6 +97,8 @@ def upload_from_path(bucket, path):
 #
 # DOWNLOAD FILES
 #
+
+# download file to path from S3
 def download_file_to_path(bucket, filename, path):
     bucketname = AWS_S3_ACCESS_KEY + "_" + bucket
     bucketname = bucketname.lower()
@@ -102,6 +111,9 @@ def download_file_to_path(bucket, filename, path):
     else:
         return False
 
+# get the download link from specific file
+# it`s only accessable when the permissons are set to public.
+# default permissions are only to the user not public
 def get_download_url(bucket, filename):
     bucketname = AWS_S3_ACCESS_KEY + "_" + bucket
     bucketname = bucketname.lower()
@@ -118,6 +130,7 @@ def get_download_url(bucket, filename):
 # DELETE FILES / CONTAINERS
 #
 
+# delete file from bucket, existing from file will be checked
 def delete_file(bucket, filename):
     ret_val = False
     bucketname = AWS_S3_ACCESS_KEY + "_" + bucket
@@ -132,6 +145,7 @@ def delete_file(bucket, filename):
         ret_val = False
     return ret_val
 
+
 def delete_container(bucket):
     ret_val = False
     bucketname = AWS_S3_ACCESS_KEY + "_" + bucket
@@ -140,7 +154,7 @@ def delete_container(bucket):
         bucket_content = s3_conn.get_bucket(bucketname)
     except S3ResponseError:
         return True
-    #delete all files in bucket before delete bucket
+    # Container must be empty before it can be delete --> delete all containing files
     for key in bucket_content.list():
         key.delete()
     s3_conn.delete_bucket(bucketname)
