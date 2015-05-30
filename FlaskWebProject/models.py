669e8a6a4d282eb7973574a0b633b1b041279cc1
Lars Richter <mail@ayeks.de>
2015-05-30 14:27:03 +0200

    modified db model to support users, groups, files and permissions. DB model only, methods are missing.

--- FlaskWebProject/models.py
+++ FlaskWebProject/models.py
@@ -1,5 +1,23 @@
 from FlaskWebProject import db
 
+userToFile = db.Table('userToFile',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('file_id', db.Integer, db.ForeignKey('file.id')),
+    db.Column('permission', db.Integer)
+)
+
+groupToFile = db.Table('groupToFile',
+    db.Column('group_id', db.Integer, db.ForeignKey('group.id')),
+    db.Column('file_id', db.Integer, db.ForeignKey('file.id')),
+    db.Column('permission', db.Integer)
+)
+
+userToGroup = db.Table('userToGroup',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('group_id', db.Integer, db.ForeignKey('group.id')),
+    db.Column('permission', db.Integer)
+)
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -7,6 +25,10 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password = db.Column(db.String(64))
     sso = db.Column(db.String(64))  # none, google, facebook
+    groups = db.relationship('Group', secondary=userToGroup,
+        backref=db.backref('users', lazy='dynamic'))
+    files = db.relationship('File', secondary=userToFile,
+        backref=db.backref('users', lazy='dynamic'))
 
     def __init__(self, username, email, password, sso):
         self.username = username
@@ -31,3 +53,22 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % (self.username)
+
+
+class File(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    folder = db.Column(db.String(120))
+    name = db.Column(db.String(120))
+    users = db.relationship('User', secondary=userToFile,
+        backref=db.backref('files', lazy='dynamic'))
+    groups = db.relationship('Group', secondary=groupToFile,
+        backref=db.backref('files', lazy='dynamic'))
+
+
+class Group(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True, unique=True)
+    users = db.relationship('User', secondary=userToGroup,
+        backref=db.backref('groups', lazy='dynamic'))
+    files = db.relationship('File', secondary=groupToFile,
+        backref=db.backref('groups', lazy='dynamic'))
