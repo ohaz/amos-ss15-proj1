19abe1850fa0b489ff8e3acc6fb3674a9a853027
Lars Richter <mail@ayeks.de>
2015-05-31 11:53:38 +0200

    added proxy extension for models, alembic wont works with them

--- FlaskWebProject/models.py
+++ FlaskWebProject/models.py
@@ -8,62 +8,13 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64))
-
-    # association proxy of "user_keywords" collection
-    # to "keyword" attribute
-    keywords = association_proxy('user_keywords', 'keyword')
-
-    def __init__(self, name):
-        self.name = name
-
-
-class UserKeyword(Base):
-    __tablename__ = 'user_keyword'
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
-    keyword_id = db.Column(db.Integer, db.ForeignKey('keyword.id'), primary_key=True)
-    special_key = db.Column(db.String(50))
-
-    # bidirectional attribute/collection of "user"/"user_keywords"
-    user = db.relationship(User,
-                backref=db.backref("user_keywords",
-                                cascade="all, delete-orphan")
-            )
-
-    # reference to the "Keyword" object
-    keyword = db.relationship("Keyword")
-
-    def __init__(self, keyword=None, user=None, special_key=None):
-        self.user = user
-        self.keyword = keyword
-        self.special_key = special_key
-
-
-class Keyword(Base):
-    __tablename__ = 'keyword'
-    id = db.Column(db.Integer, primary_key=True)
-    keyword = db.Column('keyword', db.String(64))
-
-    def __init__(self, keyword):
-        self.keyword = keyword
-
-    def __repr__(self):
-        return 'Keyword(%s)' % repr(self.keyword)
-
-
-
-
-
-"""
-class User(Base):
-    __tablename__ = 'user'
-    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password = db.Column(db.String(64))
     sso = db.Column(db.String(64))  # none, google, facebook
-    # association proxy from user to files
-    files = association_proxy('userToFile', 'userfile')
+    # association proxy of "user_userfiles" collection
+    # to "keyword" attribute
+    userfiles = association_proxy('user_userfiles', 'userfile')
 
     def __init__(self, username, email, password, sso):
         self.username = username
@@ -90,22 +41,24 @@ class User(Base):
         return '<User %r>' % (self.username)
 
 
-class userToFile(Base):
-    __tablename__ = 'userToFile'
+class UserUserfile(Base):
+    # create with:
+    # UserUserfile(Userfile("folder","file"), user, permission=7)
+    __tablename__ = 'user_userfile'
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
-    file_id = db.Column(db.Integer, db.ForeignKey('userfile.id'), primary_key=True)
-    permission = db.Column(db.String(64))
+    userfile_id = db.Column(db.Integer, db.ForeignKey('userfile.id'), primary_key=True)
+    permission = db.Column(db.String(50))
 
-    # bidirectional attribute/collection of "user"/"userToFile"
+    # bidirectional attribute/collection of "user"/"user_userfiles"
     user = db.relationship(User,
-                           backref=db.backref("userToFile",
-                                              cascade="all, delete-orphan")
-                           )
+                backref=db.backref("user_userfiles",
+                                cascade="all, delete-orphan")
+            )
 
     # reference to the "Keyword" object
     userfile = db.relationship("Userfile")
 
-    def __init__(self, user=None, userfile=None, permission=None):
+    def __init__(self, userfile=None, user=None, permission=None):
         self.user = user
         self.userfile = userfile
         self.permission = permission
@@ -114,8 +67,8 @@ class userToFile(Base):
 class Userfile(Base):
     __tablename__ = 'userfile'
     id = db.Column(db.Integer, primary_key=True)
-    folder = db.Column(db.String(120))
-    name = db.Column(db.String(120))
+    folder = db.Column('folder', db.String(64))
+    name = db.Column('name', db.String(120))
 
     def __init__(self, folder, name):
         self.folder = folder
@@ -129,9 +82,10 @@ class Userfile(Base):
 
     def __repr__(self):
         return '<Userfile %r>' % (self.name)
-"""
 
-"""class Group(db.Model):
+
+"""
+class Group(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True, unique=True)
     files = db.relationship('File', secondary=groupToFile,
