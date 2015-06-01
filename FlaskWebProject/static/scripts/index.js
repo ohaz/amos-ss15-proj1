64ae4add108f9742277f6f6cbda76361afa9723c
Huy-Long Nguyen <huy.long.nguyen26@gmail.com>
2015-06-01 09:40:33 +0200

    it is now possible to specify a filename where the calculator result should be saved

--- FlaskWebProject/static/scripts/index.js
+++ FlaskWebProject/static/scripts/index.js
@@ -4,21 +4,29 @@
 $( document ).ready(function() {
     // Handler for .ready() called.
     $( "#saveresult" ).click(function() {
-        console.log("userid: " + user_id);
-        $.ajax({
-            type: "POST",
-            data: JSON.stringify({content: $('#shared_widgets_NumberInput_2').val()}),
-            contentType: "application/json; charset=utf-8",
-            url: "/storage/api/v1.0/"+user_id+"/calculator",
-            success: function (response) {
-                if(response == "200"){
-                    alert("Result was saved")
+        //Get filename
+        var filename = $('#filename').val();
+
+        //Check if filename is not empty
+        if(filename){
+            //Save result with the given filename
+            $.ajax({
+                type: "POST",
+                data: JSON.stringify({content: $('#shared_widgets_NumberInput_2').val()}),
+                contentType: "application/json; charset=utf-8",
+                url: "/storage/api/v1.0/"+user_id+"/"+filename,
+                success: function (response) {
+                    if(response == "200"){
+                        //Hide modal after successfully saving the result
+                        $('#saveFileModal').modal('hide')
+                    }
+                },
+                error: function (data) {
+                    console.log("error in post request");
                 }
-            },
-            error: function (data) {
-                console.log("error in post request");
-            }
-        });
+            });
+        }
+
     });
     $( "#loadresult" ).click(function() {
         $.ajax({
