cc616d54b8b38e9f421a8de3b5c984f6932331d8
Lars Richter <mail@ayeks.de>
2015-06-10 01:20:33 +0200

    added testcase for register and logout, currently login after logout is buggy, will be refactored soon

---
+++ tests/functional/google/register.js
@@ -0,0 +1,48 @@
+define([
+    'intern!object',
+    'intern/chai!assert',
+    'require'
+], function (registerSuite, assert, require) {
+    registerSuite({
+        name: 'index',
+
+        // before the suite starts
+        setup: function () {
+            return this.remote
+                .setFindTimeout(5000)
+                // login
+                .findByLinkText('Sign up')
+                    .click()
+                    .end()
+                .findById('username')
+                    .click()
+                    .type('test_user' + Math.floor(Math.random() * (9999 - 100 + 1)) + 100) // not the best way to generate a test user - but it works
+                    .end()
+                .findById('email')
+                    .click()
+                    .type('email@' + (Math.floor(Math.random() * (9999 - 100 + 1)) + 100) + '.de')
+                    .end()
+                .findById('password')
+                    .click()
+                    .type('password')
+                    .end()
+                .findById('confirm')
+                    .click()
+                    .type('password')
+                    .end()
+               .findById('register_finish')
+                    .click()
+                    .end();
+        },
+
+
+        '1_2015_register': function () {
+            return this.remote
+                .getPageTitle()
+                .then(function (text) {
+                    assert.strictEqual(text, 'Netto-Lohn',
+                        'Page Header should be "Netto-Lohn" after successfull register.');
+                });
+        }
+    });
+});
