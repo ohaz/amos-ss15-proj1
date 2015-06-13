9c953d9853420423826c4e32c12c6bac17aaf263
Huy-Long Nguyen <huy.long.nguyen26@gmail.com>
2015-06-13 17:21:01 +0200

    made very first attempts to send logs to email and slack,
    -Email Server settings needs to be added
    -Posting logs to slack via HTTPHandler is not working
    -Posting to slack via normal POST request is working

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -1,14 +1,16 @@
 """
 The flask application package.
 """
+import json
 
 from flask import Flask
 from flask.ext.login import LoginManager
 from flask.ext.sqlalchemy import SQLAlchemy
+import requests
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from flask_oauthlib.client import OAuth
-from config import sso_fb_consumer_key
+from config import sso_fb_consumer_key, MAIL_USERNAME, MAIL_PASSWORD, MAIL_SERVER, MAIL_PORT, ADMINS, cloudplatform
 from config import sso_fb_consumer_secret
 from config import sso_google_consumer_key
 from config import sso_google_consumer_secret
@@ -57,6 +59,50 @@ google = oauth.remote_app(
     consumer_secret=sso_google_consumer_secret,
 )
 
+# Add SMTPHandler for Logging Errors if the app is not in debug mode
+if not app.debug:
+    import logging
+    from logging.handlers import SMTPHandler
+    credentials = None
+    if MAIL_USERNAME or MAIL_PASSWORD:
+        credentials = (MAIL_USERNAME, MAIL_PASSWORD)
+    mail_handler = SMTPHandler((MAIL_SERVER, MAIL_PORT),
+                               'no-reply@' + MAIL_SERVER, ADMINS,
+                               cloudplatform + ' failure', credentials)
+    mail_handler.setLevel(logging.ERROR)
+    app.logger.addHandler(mail_handler)
+
+class StructuredMessage(object):
+    def __init__(self, **kwargs):
+        self.kwargs = kwargs
+
+    def __str__(self):
+        return 'payload=%s' % (json.dumps(self.kwargs))
+
+_ = StructuredMessage   # optional, to improve readability
+
+# Add HTTPHandler for Logging Errors to Slack via POST request if the app is not in debug mode
+if not app.debug:
+    import logging
+    from logging.handlers import HTTPHandler
+    http_handler = logging.handlers.HTTPHandler(
+        'https://hooks.slack.com/services/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',
+        '',
+        method='POST',
+    )
+    http_handler.setLevel(logging.INFO)
+    app.logger.addHandler(http_handler)
+
+    #send test log to slack now
+    print("send log now to slack")
+    print(_(text='this is a log', channel='#logging', username=cloudplatform, icon_emoji='ghost'))
+    app.logger.info(_(text='this is a log', channel='#logging', username=cloudplatform, icon_emoji='ghost'))
+
+    #manually making a post request works
+    url = 'https://hooks.slack.com/services/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'
+    payload = {'channel': '#logging', 'text': 'this is a second log', 'username': cloudplatform, 'icon_emoji': 'ghost'}
+    headers = {'content-type': 'application/json'}
+    r = requests.post(url, data=json.dumps(payload), headers=headers)
 
 import FlaskWebProject.views
 import FlaskWebProject.models
