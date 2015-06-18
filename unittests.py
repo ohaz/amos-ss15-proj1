bcc62b496893dc63ab015bc5553143d1e3705c21
Richard Baumann <thekilleddeath@endless-aerospace.de>
2015-06-18 08:35:01 +0200

    Added a simple test for logging.
    Needs to be called with python 2.
    Also, requires a special configuration file.

---
+++ unittests.py
@@ -0,0 +1,96 @@
+import time
+import BaseHTTPServer
+import cgi
+import urlparse
+import thread
+import requests
+from FlaskWebProject import auto_logger
+from config import SLACK_HANDLER_HOST
+import sys
+import json
+
+HOST_NAME = 'localhost'
+PORT_NUMBER = int(SLACK_HANDLER_HOST.split(':')[2])
+
+search_for = {"username": "testing", "text": "Exception in [bug]: integer division or modulo by zero", "icon_emoji": "ghost", "icon": "ghost"}
+
+my_errors = []
+
+class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    def do_HEAD(s):
+        s.send_response(200)
+        s.send_header("Content-type", "text/html")
+        s.end_headers()
+    def do_GET(s):
+        global my_errors, search_for
+        """Respond to a GET request."""
+        if s.path == "/init/":
+            my_errors = []
+            s.send_response(200)
+            s.send_header("Content-type", "text/html")
+            s.end_headers()
+            s.wfile.write("")
+        elif s.path == "/":
+            if len(my_errors) > 0 and search_for in my_errors:
+                s.send_response(200)
+                s.send_header("Content-type", "text/html")
+                s.end_headers()
+                s.wfile.write(str(my_errors))
+            else:
+                s.send_response(400)
+        else:
+            s.send_response(200)
+    def do_POST(s):
+        global my_errors
+        form = cgi.FieldStorage(
+            fp=s.rfile, 
+            headers=s.headers,
+            environ={'REQUEST_METHOD':'POST',
+                     'CONTENT_TYPE':s.headers['Content-Type'],
+                     })
+        """Respond to a POST request."""
+        my_errors.append(json.loads(form.value))
+        s.send_response(200)
+        s.send_header("Content-type", "text/html")
+
+@auto_logger
+def bug():
+    a = 5/0
+
+def start_server():
+    server_class = BaseHTTPServer.HTTPServer
+    httpd = server_class((HOST_NAME, PORT_NUMBER), MyHandler)
+    try:
+            httpd.serve_forever()
+    except Exception:
+            pass
+    httpd.server_close()
+
+if __name__ == '__main__':
+    thread.start_new_thread(start_server,())
+    print(" >>> Trying to reach test server and initialize the test")
+    i = 0
+    while i < 1000:
+        try:
+            r = requests.get('http://'+':'.join([HOST_NAME,str(PORT_NUMBER)])+'/init/', data="")
+            if r.status_code == 200:
+                break
+        except Exception, e:
+            pass
+        i += 1
+    else:
+        print(" >>> Failed. Could not connect to test server")
+        sys.exit(-1)
+
+    print(" >>> Testing error logging")
+    try:
+        bug()
+    except:
+        pass
+    r = requests.get('http://'+':'.join([HOST_NAME,str(PORT_NUMBER)])+'', data="")
+    print("")
+    if r.status_code == 200:
+        print(" >>> Test [Passed]")
+    else:
+        print(" >>> Test [Failed]")
+        sys.exit(-1)
