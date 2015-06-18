a951a4c43fc0765df31e404183d3f469c005457c
Richard Baumann <thekilleddeath@endless-aerospace.de>
2015-06-18 11:09:29 +0200

    Refactored logging tests to run multiple tests
    Added wrong imports, assert errors, indexing and key errors and calls to
    functions that don't exist

--- unittests.py
+++ unittests.py
@@ -15,11 +15,96 @@ if cloudplatform != "testing":
 HOST_NAME = 'localhost'
 PORT_NUMBER = int(SLACK_HANDLER_HOST.split(':')[2])
 
-search_for = {"username": "testing", "text": "Exception in [bug]: integer division or modulo by zero", "icon_emoji": "ghost", "icon": "ghost"}
+#
+# Functions to test.
+# To add a new function do the following:
+# Write the function with the @auto_logger decorator
+# Underneath, add a try/catch that does the same thing.
+# In the catch part, call errors_append(<your_function>, str(e))
+# 
+
+search_for = []
+error_functions = []
+
+def errors_append(function, error):
+    search_for.append({"username": "testing", "text": "Exception in ["+function.__name__+"]: "+error, "icon_emoji": "ghost", "icon": "ghost"})
+    error_functions.append(function)
+
+@auto_logger
+def division_by_zero():
+    """ Cause a division by zero to test logging """
+    a = 5/0
+
+try:
+    a = 5/0
+except Exception, e:
+    errors_append(division_by_zero, str(e))
+
+@auto_logger
+def wrong_import():
+    """ Cause a import error to test logging """
+    import asuedahgwda
+
+try:
+    import asuedahgwda
+except Exception, e:
+    errors_append(wrong_import, str(e))
+
+@auto_logger
+def assert_error():
+    """ Cause an assertion error to test logging """
+    assert(True == False)
+
+try:
+    assert(True == False)
+except Exception, e:
+    errors_append(assert_error, str(e))
+
+@auto_logger
+def index_error():
+    """ Cause an indexing error """
+    a = ["hello"]
+    b = a[2]
+
+try:
+    a = ["hello"]
+    b = a[2]
+except Exception, e:
+    errors_append(index_error, str(e))
+
+@auto_logger
+def key_error():
+    """ Cause a key error in a dictionary """
+    a = {"hello": "no"}
+    b = a['error']
+
+try:
+    a = {"hello": "no"}
+    b = a['error']
+except Exception, e:
+    errors_append(key_error, str(e))
+
+@auto_logger
+def call_nonexisting_function_error():
+    asdasdkawen()
+
+def wrapper_nonexisting_function():
+    asdasdkawen()
+
+try:
+    wrapper_nonexisting_function()
+except Exception, e:
+    errors_append(call_nonexisting_function_error, str(e))
+
+
+#
+# Server side Handling
+#
 
 my_errors = []
 
 class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    """ Handler for incomming connections """
     def do_HEAD(s):
         s.send_response(200)
         s.send_header("Content-type", "text/html")
@@ -36,7 +121,12 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
             s.wfile.write("")
         elif s.path == "/":
             """ Return 200 if the error we caused exists, else return 400 """
-            if len(my_errors) > 0 and search_for in my_errors:
+            if len(my_errors) > 0:
+                for error in search_for:
+                    if not error in my_errors:
+                        print(" >>> Missing "+str(error))
+                        s.send_response(400)
+                        return
                 s.send_response(200)
                 s.send_header("Content-type", "text/html")
                 s.end_headers()
@@ -59,11 +149,6 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         s.send_response(200)
         s.send_header("Content-type", "text/html")
 
-@auto_logger
-def bug():
-    """ Cause a bug to test logging """
-    a = 5/0
-
 def start_server():
     """ Starts a server in background that waits for the logging request and saves it """
     server_class = BaseHTTPServer.HTTPServer
@@ -74,10 +159,15 @@ def start_server():
             pass
     httpd.server_close()
 
+#
+# Main part
+#
+
 if __name__ == '__main__':
     """ Start testing """
 
     # Testing logging
+    print(" >>> Starting test server")
     thread.start_new_thread(start_server,())
     print(" >>> Trying to reach test server and initialize the test")
     i = 0
@@ -86,7 +176,7 @@ if __name__ == '__main__':
             r = requests.get('http://'+':'.join([HOST_NAME,str(PORT_NUMBER)])+'/init/', data="")
             if r.status_code == 200:
                 break
-        except Exception, e:
+        except:
             pass
         i += 1
     else:
@@ -94,14 +184,15 @@ if __name__ == '__main__':
         sys.exit(-1)
 
     print(" >>> Testing error logging")
-    try:
-        bug()
-    except:
-        pass
+    for bug in error_functions:
+        try:
+            bug()
+        except:
+            pass
     r = requests.get('http://'+':'.join([HOST_NAME,str(PORT_NUMBER)])+'', data="")
     print("")
     if r.status_code == 200:
-        print(" >>> Test [Passed]")
+        print(" >>> "+str(len(error_functions))+" Tests [Passed]")
     else:
-        print(" >>> Test [Failed]")
+        print(" >>> Tests [Failed]")
         sys.exit(-1)
