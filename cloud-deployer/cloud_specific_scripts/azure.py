e6f44a45fe11aec837b021da14cf445b3192c91c
Konstantin Keuchel <konstantin.keuchel@fau.de>
2015-06-30 15:13:16 +0200

    update deploy script for azure from develop branch

--- cloud-deployer/cloud_specific_scripts/azure.py
+++ cloud-deployer/cloud_specific_scripts/azure.py
@@ -33,6 +33,10 @@ def deploy():
         return
     print(' > > Copying downloaded files into repo')
     distutils.dir_util.copy_tree(os.path.join(OWN_FOLDER, 'repo'), os.path.join(OWN_FOLDER, 'azure_repo'))
+    distutils.file_util.copy_file(os.path.join(OWN_FOLDER, 'config.py'), os.path.join(OWN_FOLDER, 'azure_repo', 'config.py'))
+    print(' Adding files to git')
+    command = ['git', 'add', '.']
+    run_subprocess(command)
     print(' > > Doing a git commit')
     command = ['git', '--git-dir='+os.path.join(OWN_FOLDER,'azure_repo', '.git/'), '--work-tree='+os.path.join(OWN_FOLDER, 'azure_repo/'), 'commit', '-am', '"Autodeploy with deployer script"']
     run_subprocess(command)
