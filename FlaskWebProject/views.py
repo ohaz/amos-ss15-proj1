48a8e3480938676e43ca16d3c50dc96263f8f14e
Konstantin Keuchel <konstantin.keuchel@fau.de>
2015-07-01 09:29:33 +0200

    add user id to file string

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -794,7 +794,7 @@ def rest_upload_from_text(bucket_id, file_name):
 
     content = request.json['content']
     etcd_client = init_etcd_connection()
-    file_string = "saveFile/" + file_name + '/'
+    file_string = "saveFile/" + user.id + '_' + file_name + '/'
 
     async_ready_queue = Queue()
     thread_listen_ready_list = []
