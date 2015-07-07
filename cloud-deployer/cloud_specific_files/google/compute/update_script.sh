98a3ea38500bc9dbb4b4c94ab9b8979571dea43e
Franz Koeferl <franz.frk.koeferl@studium.uni-erlangen.de>
2015-07-07 22:33:08 +0200

    extended the deployer for google_compute. It can now deploy local and via the branch, like the other clouds. This approach is totally diffrent, tahn the previous approach. So it is more likely to be buggy.

--- cloud-deployer/cloud_specific_files/google/compute/update_script.sh
+++ cloud-deployer/cloud_specific_files/google/compute/update_script.sh
@@ -1,31 +1,28 @@
 #!/bin/sh
 set -e
 
-branch=$1
-revision=$2
+# kill Gunicorn processes, because we are replacing the code now!
 process=gunicorn
-
-#Save environment
-#env0=$GIT_SSH
-
 bp=$(ps -A | grep $process | awk '{print $1}' | head -n 1)
 if [ -n "$bp" ] ; then
     sudo kill $bp
 fi
 
-#sudo -i su
+# get into repo-context [virtualenv]
+cp difference /home/sys/environments/amos/difference
 cd /home/sys/environments/amos/amos-ss15-proj1/
 source /home/sys/environments/amos/bin/activate
 
-sudo git pull
-sudo git checkout $branch
+#update repo 
+git checkout master
+git reset --hard HEAD
+git clean -fd --exclude=brutto-netto-rechner-compute.pem
 
+git apply ../difference
+
+#reinstall requirements if needed, 
+#sometimes, we should update the startupscript, because of really hard dependencies [gcc, etc.]
 pip install -r requirements.txt
 
+echo "Starting server ... "
 setsid ../bin/gunicorn -w 4 FlaskWebProject:app >/tmp/logging 2>&1 < /dev/null &
-
-#exit #su
-#exit #real user
-
-#restore environment
-#GIT_SSH=$env0
